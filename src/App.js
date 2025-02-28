8cf188e53815c0337b86afa40a5fbed9560f380f
Dave <discodavey@gmail.com>
2025-02-28 21:15:26 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <h1>This is a burger</h1>
+
+    </div>
+  );
+}
+
+export default App;
